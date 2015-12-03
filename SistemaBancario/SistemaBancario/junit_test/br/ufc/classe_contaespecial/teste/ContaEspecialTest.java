87ea2b93cf272e85e809987fd0c67cd524ff59c2
Rafael <rafaelsilva_93@hotmail.com>
2015-12-03 11:59:26 -0300

    ARQUIVOS_JUNIT_ADICIONADOS

---
+++ SistemaBancario/SistemaBancario/junit_test/br/ufc/classe_contaespecial/teste/ContaEspecialTest.java
@@ -0,0 +1,24 @@
+package br.ufc.classe_contaespecial.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ContaEspecialTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
