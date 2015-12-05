55882359cb3f3ee48ff90c3d78fb978309366389
Rafael <rafaelsilva_93@hotmail.com>
2015-12-05 19:36:26 -0300

    TESTES_CLASSE_CONTA_OK_ issues #11 #12 #13 #14

--- SistemaBancario/SistemaBancario/junit_test/br/ufc/classe_conta/teste/ContaTest.java
+++ SistemaBancario/SistemaBancario/junit_test/br/ufc/classe_conta/teste/ContaTest.java
@@ -5,11 +5,16 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import br.ufc.banco.conta.*;
+import br.ufc.banco.conta.excecoes.SIException;
 
 public class ContaTest {
 
+	private Conta conta;
+	
 	@Before
 	public void setUp() throws Exception {
+		conta = new Conta("12345");
 	}
 
 	@After
@@ -17,8 +22,45 @@ public class ContaTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreditarNormal() {
+		double saldo = conta.obterSaldo();
+		
+		conta.creditar(10);
+		assertEquals((saldo + 10),conta.obterSaldo(), 0);
 	}
-
+	
+	@Test
+	public void testCreditarNegativo(){
+		double saldo = conta.obterSaldo();
+		
+		conta.creditar(-10);
+		assertEquals(saldo,conta.obterSaldo(), 0);
+	}
+	
+	@Test (expected = SIException.class)
+	public void testDebitarNormal() throws SIException{
+		double saldo = conta.obterSaldo();
+		
+		conta.debitar(10);
+		assertEquals(saldo,conta.obterSaldo(),0);
+	}
+	
+	@Test
+	public void testDebitarNormalNoException() throws SIException{
+		
+		double saldo = conta.obterSaldo();
+		
+		conta.creditar(10);
+		conta.debitar(10);
+		
+		assertEquals(saldo,conta.obterSaldo(),0);
+	}
+	
+	@Test (expected = SIException.class)
+	public void testDebitarNegativo() throws SIException{
+		double saldo = conta.obterSaldo();
+		
+		conta.debitar(-10);
+		assertEquals(saldo,conta.obterSaldo(),0);
+	}
 }
