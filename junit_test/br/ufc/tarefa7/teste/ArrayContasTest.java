84d8091bc615d31f5756fb8109d07d1c12c33ceb
Priscila Rodrigues <priscila.rfr@gmail.com>
2016-01-04 15:14:14 -0300

    Update and rename BancoBrasilTest.java to ArrayContasTest.java
    Alteração nos Testes da classe ArrayContas #23

---
+++ junit_test/br/ufc/tarefa7/teste/ArrayContasTest.java
@@ -0,0 +1,65 @@
+package br.ufc.tarefa7.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.ufc.banco.conta.Conta;
+import br.ufc.banco.dados.ArrayContas;
+import br.ufc.banco.dados.excecoes.CEException;
+import br.ufc.banco.dados.excecoes.CIException;
+
+
+public class ArrayContasTest {
+
+	private ArrayContas aContas;
+	private Conta conta1;
+	
+	@Before
+	public void inicializar(){
+		aContas = new ArrayContas();
+		// criando conta
+		conta1 = new Conta("abc");
+
+	}
+	
+	@Test
+	public void testInserirContas() throws CEException{
+		int tamanhoAntes = aContas.numeroContas();
+		
+		// inserindo conta
+		aContas.inserir(conta1);
+		
+		int tamanhoDepois = aContas.numeroContas();
+		
+		assertFalse(tamanhoAntes == tamanhoDepois);	
+	}
+	
+	@Test
+	public void testProcurarConta() throws CEException{
+		String numeroConta = null;
+		
+		aContas.inserir(conta1);
+		
+		Conta conta = (Conta) aContas.procurar("abc");
+		
+		assertEquals(conta1.obterNumero(), conta.obterNumero());
+	}
+	
+	@Test
+	public void testApagarConta() throws CEException, CIException{
+		// inserindo contas
+		aContas.inserir(conta1);
+		int tamanhoAntes = aContas.numeroContas();
+		// apagando conta
+		aContas.apagar("abc");
+		
+		//verificar tamanho do Vector
+		int tamanhoDepois = aContas.numeroContas();
+		
+		assertEquals(0, tamanhoDepois);
+		
+	}
+}
+
